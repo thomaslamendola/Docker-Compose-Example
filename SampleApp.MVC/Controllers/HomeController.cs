c40b03cfb107f43366fd421f3397938607f67012
Thomas La Mendola <thomas.lamendola@gmail.com>
2020-04-21 17:06:17 +0200

    Add project files.

---
+++ SampleApp.MVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleApp.MVC.Models;
+using SampleApp.MVC.Services;
+
+namespace SampleApp.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWeatherService _weather;
+
+        public HomeController(ILogger<HomeController> logger, IWeatherService weather)
+        {
+            _logger = logger;
+            _weather = weather;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var weather = await _weather.GetWeather();
+            return View(weather);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleApp.MVC/Services/WeatherService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using SampleApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SampleApp.MVC.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private readonly HttpClient _httpClient;
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeather()
+        {
+            var uri = "/weatherforecast";
+            var responseString = await _httpClient.GetStringAsync(uri);
+            var forecast = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(responseString);
+            return forecast;
+        }
+    }
+
+    public interface IWeatherService
+    {
+        Task<IEnumerable<WeatherForecast>> GetWeather();
+    }
+}
