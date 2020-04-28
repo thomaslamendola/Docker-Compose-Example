59bde1dae486ad71af7f5d8e4e6c0460e23176e7
Thomas La Mendola <thomas.lamendola@gmail.com>
2020-04-28 12:50:23 +0200

    Updates

--- SampleApp.API/Controllers/WeatherForecastController.cs
+++ SampleApp.API/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace SampleApp.API.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Hitting API...");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
